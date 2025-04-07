bf5eee0e07da39b0d18055553b314573cdf56465
Романов Семён <shimonromanov@yandex.ru>
2025-04-08 01:20:44 +0700

    fix

--- main.py
+++ main.py
@@ -8,21 +8,14 @@ from optic_params import Trans_Intensity
 
 initial_ro = 0.9     # Коэффициент отражения
 initial_f = 3e12     # Частота (Гц)
-initial_n_real = 1.2  # Действительная часть
-initial_n_imag = 0.0  # Мнимая часть
+initial_n = 1.2      # Показатель преломления плёнки
+initial_kappa = 0    # Коэффициент поглощения
 initial_d = 3e-5     # Толщина плёнки (м)
 
 fig, ax = plt.subplots()
 plt.subplots_adjust(left=0.1, right=0.75, bottom=0.4)
 
-trans_intensity = Trans_Intensity(
-    ro=initial_ro, 
-    f=initial_f, 
-    n_real=initial_n_real, 
-    n_imag=initial_n_imag, 
-    d=initial_d
-)
-
+trans_intensity = Trans_Intensity(ro=initial_ro, f=initial_f, n=initial_n, kappa=initial_kappa, d=initial_d)
 
 # Диапазон значений Δ
 delta_values = np.linspace(1e-4, 1e-3, 100000)
@@ -37,49 +30,13 @@ ax.grid(True)
 cursor = Cursor(ax, horizOn=True, vertOn=True, color='r', lw=1)
 
 
+ax_ro = plt.axes([0.2, 0.10, 0.65, 0.03])
 ax_freq = plt.axes([0.2, 0.25, 0.65, 0.03])
-ax_n_real = plt.axes([0.2, 0.20, 0.65, 0.03])
-ax_n_imag = plt.axes([0.2, 0.15, 0.65, 0.03])
-ax_d = plt.axes([0.2, 0.10, 0.65, 0.03])
-ax_ro = plt.axes([0.2, 0.05, 0.65, 0.03])
+ax_n = plt.axes([0.2, 0.20, 0.65, 0.03])
+ax_kappa = plt.axes([0.2, 0.05, 0.65, 0.03])
+ax_d = plt.axes([0.2, 0.15, 0.65, 0.03])
 
 ax_print = plt.axes([0.85, 0.6, 0.05, 0.05])
-
-slider_f = Slider(
-    ax=ax_freq,
-    label='Частота (ТГц)',
-    valmin=1,
-    valmax=10,
-    valinit=initial_f * (10 ** -12),
-    valstep=0.01
-)
-
-slider_n_real = Slider(
-    ax=ax_n_real,
-    label='Re(n)',
-    valmin=1.0,
-    valmax=3.0,
-    valinit=initial_f * (10 ** -12),
-    valstep=0.01
-)
-
-slider_n_imag = Slider(
-    ax=ax_n_imag,
-    label='Im(n)',
-    valmin=0.0,
-    valmax=1.0,
-    valinit=initial_n_imag,
-    valstep=0.01
-)
-
-slider_d = Slider(
-    ax=ax_d,
-    label='Толщина плёнки (мкм)',
-    valmin=1,
-    valmax=300,
-    valinit=initial_d * (10 ** 6),
-    valstep=1
-)
 
 slider_ro = Slider(
     ax=ax_ro,
@@ -88,6 +45,42 @@ slider_ro = Slider(
     valmax=0.9,
     valinit=initial_ro,
     valstep=0.1
+)
+
+slider_f = Slider(
+    ax=ax_freq,
+    label='Частота (ТГц)',
+    valmin=1,
+    valmax=10,
+    valinit=initial_f * (10 ** -12),
+    valstep=0.1
+)
+
+slider_n = Slider(
+    ax=ax_n,
+    label='Показатель преломления',
+    valmin=1.0,
+    valmax=3.0,
+    valinit=initial_n,
+    valstep=0.1
+)
+
+slider_kappa = Slider(
+    ax=ax_kappa,
+    label='Коэффициент поглощения',
+    valmin=0,
+    valmax=1,
+    valinit=initial_kappa,
+    valstep=0.1
+)
+
+slider_d = Slider(
+    ax=ax_d,
+    label='Толщина плёнки (мкм)',
+    valmin=1,
+    valmax=900,
+    valinit=initial_d * (10 ** 6),
+    valstep=1
 )
 
 button_print = Button(ax_print, 'Печать')
@@ -111,20 +104,24 @@ def save_peaks(event):
 button_print.on_clicked(save_peaks)
 
 def update(val):
-    trans_intensity.f = slider_f.val * 1e12
-    trans_intensity.n_real = slider_n_real.val
-    trans_intensity.n_imag = slider_n_imag.val
-    trans_intensity.d = slider_d.val * 1e-6
     trans_intensity.ro = slider_ro.val
-    trans_intensity.R = trans_intensity.ro**2
-    trans_intensity.F = 4 * trans_intensity.R / (1 - trans_intensity.R)**2
+    trans_intensity.f = slider_f.val * (10 ** 12)
+    trans_intensity.n = slider_n.val
+    trans_intensity.kappa = slider_kappa.val
+    trans_intensity.d = slider_d.val * (10 ** -6)
+    
+    trans_intensity.update_R_absorb()
+    trans_intensity.update_F()
+
     line.set_ydata(trans_intensity(delta_values))
     fig.canvas.draw_idle()
 
+
 slider_f.on_changed(update)
-slider_n_real.on_changed(update)
-slider_n_imag.on_changed(update)
-slider_d.on_changed(update)
 slider_ro.on_changed(update)
+slider_n.on_changed(update)
+slider_kappa.on_changed(update)
+slider_d.on_changed(update)
+
 
 plt.show()
--- optic_params.py
+++ optic_params.py
@@ -2,19 +2,32 @@ import numpy as np
 from scipy import constants
 
 class Trans_Intensity:
-    def __init__(self, ro: float, f: float, n_real: float, n_imag: float, d: float):
+    def __init__(self,
+                 ro: float,
+                 f: float,
+                 n: float,
+                 kappa : float,
+                 d: float):
         self.ro = ro
         self.R = ro**2
         self.f = f
-        self.n_real = n_real
-        self.n_imag = n_imag
+        self.n = n
+        self.kappa = kappa
         self.d = d
-        self.F = 4 * self.R / ((1 - self.R)**2)
+        self.A = np.exp(-4 * np.pi * self.kappa * self.d * self.f / constants.c)
+        self.R_absorb = self.R * self.A
+        self.F = 4 * self.R_absorb / ((1 - self.R_absorb)**2)
     
     def __call__(self, delta: np.ndarray) -> np.ndarray:
-        phase = (2 * np.pi * self.f / constants.c) * (delta + self.d * self.n_real)
-        absorption = np.exp(-4 * np.pi * (self.f / constants.c) * self.d * self.n_imag) # https://en.wikipedia.org/wiki/Refractive_index#Complex_refractive_index
-        return absorption / (1 + self.F * np.sin(phase)**2)
+        phase = (2 * np.pi * self.f / constants.c) * (delta + self.d * self.n)
+        return self.A / (1 + self.F * np.sin(phase)**2)
     
     def get_intensity(self, delta: np.ndarray) -> np.ndarray:
         return self(delta)
+    def update_R_absorb(self):
+        self.R = self.ro**2
+        self.A = np.exp(-4 * np.pi * self.kappa * self.d * self.f / constants.c)
+        self.R_absorb = self.R * self.A
+
+    def update_F(self):
+        self.F = 4 * self.R_absorb / ((1 - self.R_absorb)**2)
